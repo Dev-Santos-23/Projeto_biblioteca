f17e501c9cd992937bd3ddd0c8ee669f4b8696a6
Lucas Dos Santos Nascimento <lucas.nascimento.engineer@gmail.com>
2026-07-12 09:34:21 -0300

    Criando conexão com o banco

---
+++ Project_blicioteca/take_conection.py
@@ -0,0 +1,30 @@
+import os
+import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
+
+senha = os.getenv("DB_PASSWORD")
+
+def conectar():
+    try:
+        conn = psycopg2.connect(
+            host = "localhost",
+            port = "5432",
+            user = "postgres",
+            password = senha,
+            database = "loja_de_livros"
+        )
+        print("Conectado com Sucesso!")
+        
+        return conn
+    
+    except Exception as e:
+        print(f"Erro ao conectar: {e}")
+        
+def _close(conn):
+    if conn:
+        conn.close()
+    print("Conexão encerrada!")
+    
+               
