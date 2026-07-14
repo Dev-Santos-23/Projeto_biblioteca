c7fb4295265520633c4ecf2e65375a0198ce1967
Lucas Dos Santos Nascimento <lucas.nascimento.engineer@gmail.com>
2026-07-14 11:36:54 -0300

    finalizando a organização

--- Project_blicioteca/cadastros/create_autores.py
+++ Project_blicioteca/cadastros/create_autores.py
@@ -1,20 +1,16 @@
-from take_conection import conectar, _close
-from Project_blicioteca.tests_and_validations.back_of_create_autores import buscar_pais, validar_autor
+"""  Como rodar o projeto no seu vscode:
+ 
+    Primiero rode no terminal:
+    cd "C:\Users\Lucas\Desktop\Nova pasta (2)\Project_blicioteca"
+    
+    Depois rode:
+    python -m casdastro.create_autores
+    
+"""
 
-def injetando_autores_no_banco(inject):
-    
-    try:
-        conection = conectar()
-        cursor = conection.cursor()
-        
-        cmd_select = "INSERT INTO autores (nome, pais) VALUES (%s, %s)"
-        values = inject
-        cursor.execute(cmd_select, values)
-        conection.commit()
-        _close(conection)
-    
-    except Exception as e:
-        print(f"Não foi possivel fazer a conexão: {e}")
+from comandos_SQL_postgre import comandos
+from tests_and_validations.back_of_create_autores import buscar_pais, validar_autor
+
         
 def cadastro_autor():
     
@@ -25,7 +21,7 @@ def cadastro_autor():
     pais_autor = buscar_pais(pais_autor)
     
     inject_values = autor, pais_autor
-    injetando_autores_no_banco(inject_values)
+    comandos.injetando_autores_no_banco(inject_values)
 
 if __name__ == "__main__":
     
