9d81a5b61efd5c86f02239a10a4cc6b9518a15bc
Lucas Dos Santos Nascimento <lucas.nascimento.engineer@gmail.com>
2026-07-16 20:08:46 -0300

    criação das editoras

---
+++ Project_blicioteca/cadastros/create_editoras.py
@@ -0,0 +1,10 @@
+"""  Como rodar o projeto no seu vscode:
+ 
+    Primiero rode no terminal:
+    cd "C:\Users\Lucas\Desktop\Nova pasta (2)\Project_blicioteca"
+    
+    Depois rode:
+    python -m casdastro.create_editoras
+    
+"""
+
--- Project_blicioteca/comandos_SQL_postgre/comandos.py
+++ Project_blicioteca/comandos_SQL_postgre/comandos.py
@@ -14,3 +14,18 @@ def injetando_autores_no_banco(inject):
     
     except Exception as e:
         print(f"Não foi possivel fazer a conexão: {e}")
+        
+def injetando_editoras_no_banco(inject):
+    
+    try:
+        conection = conectar()
+        cursor = conection.cursor()
+        
+        cmd_select = "INSERT INTO autores (nome, pais) VALUES (%s, %s)"
+        values = inject
+        cursor.execute(cmd_select, values)
+        conection.commit()
+        _close(conection)
+    
+    except Exception as e:
+        print(f"Não foi possivel fazer a conexão: {e}")        
